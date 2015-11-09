e7ea96cbb9d847c59f2ef207abac1646cbbe9a81
Cade Ward <cadebward@gmail.com>
2015-11-09 13:10:25 -0600

    update index with csv code

--- index.js
+++ index.js
@@ -1,38 +1,38 @@
 var axios = require('axios')
-var secrets = require('./secrets.json')
+var papa = require('papaparse')
+var fs = require('fs')
 
-// # Steps for Exercise 1:
+var apiKey = ''
 
-// - Get a user api key
-var apiKey = secrets.apiKey
+var csv = fs.readFileSync('./csvs/MOCK_DATA.csv', 'utf8')
 
-// - Set the authentication header
-var headers = {
-  Authorization: 'Bearer ' + apiKey
-}
+papa.parse(csv, {
+  header: true,
+  step: function(results, parser) {
+    var data = results.data[0]
+    var course = {
+      title: data.title,
+      subjectCode: data.subject,
+      status: 'active',
+      number: data.number,
+      description: data.description,
+      startTerm: {
+        type: 'year',
+        year: '2016'
+      }
+    }
 
-// - Get some course data
-var course = {
-  subjectCode: 'SHIRE',
-  number: '101',
-  status: 'active',
-  title: 'A long expected party',
-  description: 'Instruction on how to best celebrate an elenty-first birthday',
-  startTerm: {
-    type: 'year',
-    year: '2015'
+    axios.post(
+      'https://frodo-stg.kuali.co/api/cm/courses',
+      course,
+      {headers: headers}
+    )
+    .then(function(res) {
+      console.log('inserted: ', res.data)
+    })
+    .catch(function(e) {
+      console.error('Error: ', e)
+    })
+
   }
-}
-
-// - Post it to the API
-axios.post(
-  'https://frodo-stg.kuali.co/api/cm/courses',
-  course,
-  {headers: headers}
-)
-.then(function(res) {
-  console.log('inserted: ', res.data)
-})
-.catch(function(e) {
-  console.error('Error: ', e)
 })
